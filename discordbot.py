8faf67289719949e33b3acfc6c3c3f546e6221c8
Green256 <56480525+Green256@users.noreply.github.com>
2019-10-22 17:10:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,12 @@
 from discord.ext import commands
-import os,traceback,discord,random,asyncio,time
+import os,traceback,discord,random,asyncio,time,sys
 from discord.ext.commands import CommandNotFound
 client = discord.Client()
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+
 #━━ エラー ━━#
-#ゴリラ
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
@@ -20,6 +20,7 @@ async def on_command_error(ctx, error):
         return
     raise error
 
+#━━━管理系━━━#    
 @bot.command()
 async def ping(ctx):
     before = time.monotonic()
@@ -28,5 +29,16 @@ async def ping(ctx):
     ping = (time.monotonic() - before) * 1000
     embed = discord.Embed(description=f"Pong!\n`反応速度:`{int(ping)}ms")
     await msg.edit(embed=embed)
-
+    
+@bot.command()
+async def restart(ctx):
+    if ctx.author.id == 462557598231560193:
+        embed=discord.Embed(title="強制再起動を開始します.",color=discord.Color(random.randint(0,0xFFFFFF)))
+        await ctx.send(embed=embed)
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+        
+    else: embed=discord.Embed(title="このBOTの管理者以外は使用することができません。",color=discord.Color(random.randint(0,0xFFFFFF)))
+    await ctx.send(embed=embed)
+    
 bot.run(token)
